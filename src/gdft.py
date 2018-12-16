Fixed correlations fns, corrected sign for W_N and wrote new tests for opitmizer

src/gdft.py
@@ -4,7 +4,7 @@ from scipy.linalg import expm
 
 
 def dft_matrix(dim):
-    W = np.exp(1j * 2 * np.pi / dim)
+    W = np.exp(-1j * 2 * np.pi / dim)
 
     def get_el(row_ind, col_ind):
         return W ** (row_ind * col_ind)

test/testOptimizer.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append("../src")
 import unittest
 import numpy as np
+from tools import *
 from gdft import *
 from correlations import *
 from optimizer import *
@@ -19,55 +20,42 @@ class TestOptimizer(unittest.TestCase):
         self.assertAlmostEqual((0.5, 0.5, 0.5, 0.5, 2.0), correlations)
 
     def test_get_random_gdft(self):
-        gdft = self.optimizer.get_random_gdft(2)
-        #print(gdft)
+        gdft = self.optimizer.get_random_gdft(8)
+        self.assertTrue(abs(np.mean(gdft)) < 0.2)
         #print(gdft*np.transpose(np.conjugate(gdft)))
-
-    def test_generate_results(self):
-        '''gdfts = np.zeros((8,8,50), dtype=np.complex128)
-        for n in range(50):
-            gdfts[:, :, n] = self.optimizer.get_random_gdft(8)
-            #print(self.optimizer.get_random_gdft())
-
-        correlations = np.zeros((50, 4), dtype=np.complex128)
-        for n in range(50):
-            correlations[n, :] = self.optimizer.get_correlations(gdfts[:, :, n])
-        print(correlations)
-        #print(self.optimizer.generate_results(20))'''
-        dft8 = dft_matrix(8)
-
-        correlations = self.optimizer.get_correlations(dft8)
-        #print(correlations)
 
 
     def test_calc_correlation(self):
         params = [0]*16
         R_ac = self.optimizer._calc_correlation(8, params, avg_auto_correlation)
         self.assertEqual(R_ac, avg_auto_correlation(corr_tensor(dft_matrix(8))))
-        thetas = np.array([0.39141802, 0.4717793 , 0.49257769, 0.52124477, 2.28552077,
-                           3.27810469, 4.8816465 , 5.72360472])
+
+    def test_corr_deps_on_params(self):
+        thetas = np.array([0.39141802, 0.4717793, 0.49257769, 0.52124477, 2.28552077,
+                           3.27810469, 4.8816465, 5.72360472])
 
         gammas = np.array([0.28036933, 1.06012391, 1.26076182, 2.30176797, 2.75600197,
-                           3.21080787, 4.04318979,4.7630171])
-        gdft = gdft_matrix(8, thetas, gammas)
-        print(gdft[0, :])
-        correlations = self.optimizer.get_correlations(gdft)
-        print(correlations)
+                           3.21080787, 4.04318979, 4.7630171])
 
+        gdft = gdft_matrix(8, thetas, gammas)
+        old_correlations = self.optimizer.get_correlations(gdft)
         gammas = np.ones(8)
-        gdft = gdft_matrix(8, thetas, thetas)
-        print(gdft[0, :])
-        correlations = self.optimizer.get_correlations(gdft)
-        print(correlations)
+        new_gdft = gdft_matrix(8, thetas, thetas)
+        new_correlations = self.optimizer.get_correlations(new_gdft)
+        for new_corr, old_corr in zip(new_correlations, old_correlations):
+            self.assertAlmostEqual(new_corr, old_corr)
+        self.assertNotEqual(new_gdft[0, 0], gdft[0, 0])
+        self.assertTrue(AlmostEqualMatrices(np.dot(new_gdft, np.conjugate(new_gdft)), 8*np.identity(8)))
+
 
     '''def test_optimize_avg_cross_corr(self):
         params0 = self.optimizer._optimize_corr_fn(8, avg_auto_correlation)
         parameters = self.optimizer._optimize_corr_fn(8, avg_auto_correlation, init_guess=params0[0])
         ordered_params = self.optimizer._order_results(parameters)
-        print(ordered_params[0])'''
+        print(ordered_params[0])
 
 
-    '''def test_optimize_avg_cross_corr_with_cycles(self):
+    def test_optimize_avg_cross_corr_with_cycles(self):
         params0 = self.optimizer.optimize_corr_fn(8, avg_auto_correlation)
         params = self.optimizer._order_results(params0)
         print(params)