2490a38949dec72e0836e4717cbe40072d25bbad
DogeMajor <tepsalainen@gmail.com>
2019-01-16 14:52:30 +0200

    Wrote a plotter as decorator

--- test/tools.py
+++ test/tools.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 
 def EqualMatrices(matA, matB):
     are_equal = True
@@ -8,6 +10,7 @@ def EqualMatrices(matA, matB):
             are_equal = are_equal and bool(matA[row, col] == matB[row, col])
     return are_equal
 
+
 def AlmostEqualMatrices(matA, matB, decimals=7):
     are_equal = True
     if matA.shape != matB.shape:
@@ -16,3 +19,18 @@ def AlmostEqualMatrices(matA, matB, decimals=7):
         for col in range(matA.shape[1]):
             are_equal = are_equal and bool(round(matA[row, col], decimals) == round(matB[row, col], decimals))
     return are_equal
+
+
+def show_plot(wait=3, close=True):
+
+    def _show_plot(fn):
+        title = fn.__name__
+        def plot_fn(self, *args, **kwargs):
+            fn(self, *args, **kwargs)
+            plt.title(title)
+            plt.pause(wait)
+            if close:
+                plt.close()
+            plt.show()
+        return plot_fn
+    return _show_plot
